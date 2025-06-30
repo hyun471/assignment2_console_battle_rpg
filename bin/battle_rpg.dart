f6f84e765cb641e98cfc72efd1df3e3a8f11367a
hyun471 <khs101ttl@gmail.com>
2025-06-30 11:13:02 +0900

    first commit

---
+++ bin/battle_rpg.dart
@@ -0,0 +1,5 @@
+import 'package:battle_rpg/battle_rpg.dart' as battle_rpg;
+
+void main(List<String> arguments) {
+  print('Hello world: ${battle_rpg.calculate()}!');
+}
---
+++ test/battle_rpg_test.dart
@@ -0,0 +1,8 @@
+import 'package:battle_rpg/battle_rpg.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
