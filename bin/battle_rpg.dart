c8781dfe9cdc03a47f894c185532bffe95ad05fb
hyun471 <khs101ttl@gmail.com>
2025-06-30 11:18:43 +0900

    시나리오 주석으로 추가

--- bin/battle_rpg.dart
+++ bin/battle_rpg.dart
@@ -1,5 +1,15 @@
-import 'package:battle_rpg/battle_rpg.dart' as battle_rpg;
-
-void main(List<String> arguments) {
-  print('Hello world: ${battle_rpg.calculate()}!');
+void main() {
+  print('Hello world!');
 }
+
+// 캐릭터 정보 클래스 생성
+// ㄴ 캐릭터 정보(이름, 체력, 공격력, 방어력) 파일에서 추출
+// ㄴ 공격 메서드, 방어 메서드, 상태(체력, 공격력, 방어력) 메서드
+
+// 몬스터 정보 클래스 생성
+// ㄴ 몬스터 정보(이름, 체력, 공격력(공격력 >= 플레이어 방어), 방어력(0)) 파일에서 추출
+// ㄴ 공격 메서드, 체력 상태(체력, 공격력, 방어력) 메서드
+
+// 게임 시작 클래스 생성
+// ㄴ 캐릭터, 몬스터, 라운드(물리친 몬스터 수)
+// ㄴ 게임시작 메서드, 전투 진행 메서드, 랜덤 몬스터 불러오기 메서드
--- test/battle_rpg_test.dart
+++ test/battle_rpg_test.dart
@@ -1,8 +1,1 @@
-import 'package:battle_rpg/battle_rpg.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
